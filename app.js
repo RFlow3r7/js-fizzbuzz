f246dc6340e8b762c78366f64d20c469755de729
RFlow3r7 <andrefiore19967@gmail.com>
2024-03-13 22:42:22 +0100

    Edit: Commenti

---
+++ app.js
@@ -0,0 +1,18 @@
+// Iniziliazzo il ciclo for //
+for (let i = 1; i <= 100; i++) {
+
+    // Multipli di 3 e 5 //
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    // Calcolo dei multipli di 3 //
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+    // Multipli di 5 //
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+
+    }
+    console.log(i);
+}
